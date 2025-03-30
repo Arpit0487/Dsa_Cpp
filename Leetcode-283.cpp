3cab8896540eda02f3a00b980805c471b8741384
Arpit0487 <arpitsh5568@gmail.com>
2025-03-30 22:37:25 +0530

    Move zeros

--- Leetcode-283.cpp
+++ Leetcode-283.cpp
@@ -1,7 +1,7 @@
 class Solution {
     public:
         void moveZeroes(vector<int>& nums) {
-          vector<int> temp ; 
+          vector<int> temp; 
           for(int i = 0 ; i<nums.size() ; i++){
             if(nums[i] != 0) temp.push_back(nums[i]) ; 
           }
