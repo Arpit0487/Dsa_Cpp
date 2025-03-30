9756c77019cefe5283f30d9e1a3d980cf7d24a1c
Arpit0487 <arpitsh5568@gmail.com>
2025-03-30 22:36:43 +0530

    Move zeros

---
+++ Leetcode-283.cpp
@@ -0,0 +1,14 @@
+class Solution {
+    public:
+        void moveZeroes(vector<int>& nums) {
+          vector<int> temp ; 
+          for(int i = 0 ; i<nums.size() ; i++){
+            if(nums[i] != 0) temp.push_back(nums[i]) ; 
+          }
+           for(int i = 0 ; i<nums.size() ; i++){
+            if(nums[i] == 0) temp.push_back(nums[i]) ; 
+          }
+          nums.clear() ; 
+          nums = temp ; 
+        }
+    };
