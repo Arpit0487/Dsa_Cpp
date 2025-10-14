0e090f02a1c00978347e1db2a64e332ab76c2fb0
Arpit Sharma <arpitsh5568@gmail.com>
2025-10-14 22:06:21 +0530

    Update Leetcode-283.cpp

--- Leetcode-283.cpp
+++ Leetcode-283.cpp
@@ -12,3 +12,21 @@ class Solution {
           nums = temp ; 
         }
     };
+
+//another method
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+      vector<int> temp ; 
+      int count = 0;
+      for(int i = 0 ; i<nums.size() ; i++){
+        if(nums[i] != 0) temp.push_back(nums[i]) ;
+        else if(nums[i]==0) count++; 
+      }
+      while(count--){
+        temp.push_back(0);
+      }
+      nums.clear() ; 
+      nums = temp ; 
+    }
+};
